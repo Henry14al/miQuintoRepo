d468e23c29a5c99b708c2ded001e062d2fda805b
henry14al <henry14al@gmail.com>
2024-11-17 07:53:10 -0500

    primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="miQuintoRepo",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Henry Vasquez",                         # Tu nombre
+    author_email="hvasquezr1@ucvvirtual.edu.pe",    # Tu correo electrónico
+    url="https://github.com/henry14al/miquintorepo",     # URL del proyecto
+)
